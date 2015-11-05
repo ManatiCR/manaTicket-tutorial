846bb33439fd561a0dd26baef3d888a737f06d05
Federico Figueroa <ffc.figueroa@gmail.com>
2015-11-05 03:41:58 -0600

    Add extra folder for 3rd party code in client/components

--- Gruntfile.js
+++ Gruntfile.js
@@ -143,7 +143,8 @@ module.exports = function (grunt) {
       all: [
         '<%= yeoman.client %>/{app,components}/**/*.js',
         '!<%= yeoman.client %>/{app,components}/**/*.spec.js',
-        '!<%= yeoman.client %>/{app,components}/**/*.mock.js'
+        '!<%= yeoman.client %>/{app,components}/**/*.mock.js',
+        '!<%= yeoman.client %>/{app,components}/extra/**/*.js'
       ],
       test: {
         src: [
